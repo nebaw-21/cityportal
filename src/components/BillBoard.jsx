487295dc93ab8e7243cec85cd5ad1d958d944705
Biruk Lemma <biruklemmadebela@gmail.com>
2024-12-19 14:55:41 +0300

    some changes

--- src/components/BillBoard.jsx
+++ src/components/BillBoard.jsx
@@ -19,19 +19,17 @@ const Billboard = () => {
 
       {/* Billboard Text Content */}
       <MaxWidth>
-      <div className="absolute inset-y-0 left-[10%] sm:left-[15%] flex items-center justify-start px-8 sm:px-16 z-0">
-        <div className="text-white max-w-lg">
-          <h1 className="text-4xl sm:text-5xl font-bold mb-4">
-            Sheger city
-          </h1>
-          <h1 className="text-4xl sm:text-5xl font-bold mb-4">
-          Administration
-          </h1>
- 
+        <div className="absolute inset-y-0 left-[10%] sm:left-[15%] flex items-center justify-start sm:px-16 md:px-10 z-0">
+          <div className="text-white max-w-lg">
+            <h1 className="text-4xl sm:text-5xl font-bold mb-4">
+              Sheger city
+            </h1>
+            <h1 className="text-4xl sm:text-5xl font-bold mb-4">
+            Administration
+            </h1>
+  
+          </div>
         </div>
-      </div>
-
-     
       </MaxWidth>
 
     </div>
@@ -39,3 +37,7 @@ const Billboard = () => {
 };
 
 export default Billboard;
+
+// eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJsb2NhdGlvbl9pZCI6IlZveUlHdDFCNEp2TVZzMFZXRzhSIiwidmVyc2lvbiI6MSwiaWF0IjoxNzMzNjYxMzAyMDU5LCJzdWIiOiJFakhvSUpvUW1xYXhScnlLaEVCWiJ9.pBaS1euB1zrFb3oGaTTOMBjP5eRyuMhQVKhdQ8BGp7o
+//Cabinet  https://cdn.courses.apisystem.tech/memberships/B4AUUrc6WphiyeYSrnUG/post-materials/a876161a-5416-4a96-ba82-cfb9bc0397b6/CabinetGrotesk-Regular-d0pn.woff2
+//NeueMontreal  https://cdn.courses.apisystem.tech/memberships/B4AUUrc6WphiyeYSrnUG/post-materials/a876161a-5416-4a96-ba82-cfb9bc0397b6/NeueMontreal-Regular-dy5w.woff2
--- src/components/decorations/DashBox.jsx
+++ src/components/decorations/DashBox.jsx
@@ -4,7 +4,7 @@ import React from 'react'
 // eslint-disable-next-line react/prop-types
 const DashBox = ({customStyles}) => {
   return (
-    <div className={"w-[100px] h-[8px] bg-[#ff0000] absolute bottom-[18px]  " + customStyles}></div>
+    <div className={"w-[100px] h-[8px] bg-[rgb(255,0,0)] absolute bottom-[18px]  " + customStyles}></div>
   )
 }
 
